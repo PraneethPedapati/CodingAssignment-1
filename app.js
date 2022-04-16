a70399e360d12adcfc23715ff8238ec4aaf35ca2
Praneeth Pedapati <praneethpedapati98@gmail.com>
2022-04-16 16:35:12 +0000

    errors

--- app.js
+++ app.js
@@ -31,6 +31,23 @@ let initializeDBAndServer = async () => {
 
 initializeDBAndServer();
 
+let convertToResponseObj = (data) => {
+  resultArray = [];
+  let obj = {};
+  for (let item of data) {
+    obj = {
+      id: item.id,
+      todo: item.todo,
+      priority: item.priority,
+      status: item.status,
+      category: item.category,
+      dueDate: item.due_date,
+    };
+    resultArray.push(obj);
+  }
+  return resultArray;
+};
+
 let hasStatusAndPriorityStatus = (requestQuery) => {
   return (
     requestQuery.status !== undefined && requestQuery.priority !== undefined
@@ -61,6 +78,27 @@ let hasCategoryAndPriorityProperty = (requestQuery) => {
   );
 };
 
+let updateStatus = (requestQuery) => {
+  return requestQuery.status !== undefined;
+};
+
+let updateTodo = (requestQuery) => {
+  return requestQuery.todo !== undefined;
+};
+
+let updateCategory = (requestQuery) => {
+  return requestQuery.category !== undefined;
+};
+
+let updateDuedate = (requestQuery) => {
+  return requestQuery.dueDate !== undefined;
+};
+
+let updatePriority = (requestQuery) => {
+  return requestQuery.priority !== undefined;
+};
+
+//API-1
 app.get("/todos/", async (request, response) => {
   let { status, priority, search_q = "", category } = request.query;
   let data = null;
@@ -101,7 +139,7 @@ app.get("/todos/", async (request, response) => {
             FROM todo
             WHERE todo LIKE "%${search_q}%"
             AND status = "${status}"
-            AND category = "${category};
+            AND category = "${category}";
           `;
       break;
 
@@ -110,7 +148,7 @@ app.get("/todos/", async (request, response) => {
             SELECT *
             FROM todo
             WHERE todo LIKE "%${search_q}%"
-            AND category = "${category};
+            AND category = "${category}";
           `;
       break;
 
@@ -120,7 +158,7 @@ app.get("/todos/", async (request, response) => {
             FROM todo
             WHERE todo LIKE "%${search_q}%"
             AND category = "${category}"
-            AND priority = "${priority};
+            AND priority = "${priority}";
           `;
       break;
 
@@ -132,9 +170,10 @@ app.get("/todos/", async (request, response) => {
           `;
   }
   data = await db.all(getTodosQuery);
-  response.send(data);
+  response.send(convertToResponseObj(data));
 });
 
+//API-2
 app.get("/todos/:todoId/", async (request, response) => {
   let { todoId } = request.params;
   let getTodoQuery = `
@@ -143,10 +182,11 @@ app.get("/todos/:todoId/", async (request, response) => {
         WHERE id = ${todoId};
     `;
 
-  let todo = await db.get(getTodoQuery);
-  response.send(todo);
+  let todo = await db.all(getTodoQuery);
+  response.send(convertToResponseObj(todo)[0]);
 });
 
+//API-3
 app.get("/agenda/", async (request, response) => {
   let { date } = request.query;
   let getDuedateQuery = `
@@ -156,9 +196,88 @@ app.get("/agenda/", async (request, response) => {
   `;
 
   let todo = await db.all(getDuedateQuery);
-  response.send(todo);
+  response.send(convertToResponseObj(todo));
 });
 
+//API-4
 app.post("/todos/", async (request, response) => {
   let { id, todo, priority, status, category, dueDate } = request.body;
+  let addTodoQuery = `
+    INSERT INTO todo
+    (id, todo, priority, status, category, due_date)
+    VALUES (${id}, "${todo}", "${priority}", "${status}", "${category}", "${dueDate}");
+  `;
+
+  db.run(addTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+//API-5
+app.put("/todos/:todoId/", async (request, response) => {
+  let { todoId } = request.params;
+  let { status, todo, category, priority, dueDate } = request.body;
+  let updateTodoQuery = "";
+  switch (true) {
+    case updateStatus(request.body):
+      updateTodoQuery = `
+    UPDATE todo
+    SET status = "${status}"
+    WHERE id = ${todoId};
+  `;
+      await db.run(updateTodoQuery);
+      response.send("Status Updated");
+      break;
+
+    case updatePriority(request.body):
+      updateTodoQuery = `
+    UPDATE todo
+    SET priority = "${priority}"
+    WHERE id = ${todoId};
+  `;
+      await db.run(updateTodoQuery);
+      response.send("Priority Updated");
+      break;
+
+    case updateTodo(request.body):
+      updateTodoQuery = `
+    UPDATE todo
+    SET todo = "${todo}"
+    WHERE id = ${todoId};
+  `;
+      await db.run(updateTodoQuery);
+      response.send("Todo Updated");
+      break;
+
+    case updateCategory(request.body):
+      updateTodoQuery = `
+    UPDATE todo
+    SET category = "${category}"
+    WHERE id = ${todoId};
+  `;
+      await db.run(updateTodoQuery);
+      response.send("Status Updated");
+      break;
+
+    case updateDuedate(request.body):
+      updateTodoQuery = `
+    UPDATE todo
+    SET due_date = "${dueDate}"
+    WHERE id = ${todoId};
+  `;
+      await db.run(updateTodoQuery);
+      response.send("Due Date Updated");
+      break;
+  }
+});
+
+//API-6
+app.delete("/todos/:todoId/", async (request, response) => {
+  let { todoId } = request.params;
+  let deleteTodoQuery = `
+    DELETE todo
+    WHERE id = ${todoId}
+  `;
+
+  db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
 });
